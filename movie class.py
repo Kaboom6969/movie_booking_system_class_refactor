51a5bfbe876f8a9590f92c545d2e7197e90fb152
BGORE <leezhihao0427@gmail.com>
2025-11-21 00:24:11 +0800

    refactor all the class, now the MovieManger got father class BaseManager and Movie got father class BaseEntity

--- movie class.py
+++ movie class.py
@@ -1,11 +1,58 @@
+import csv
 from datetime import datetime
+from abc import ABC, abstractmethod
+
+class BaseManager(ABC):
+    def __init__(self):
+        self.datas = []
+
+    def add(self,entity):
+        if self.get(entity.primary_key): raise ValueError ("Error From class BaseManager: got same primary key in the Manager")
+        self.datas.append(entity)
+
+    def remove(self, data_code):
+        for data in self.datas:
+            if data.primary_key == data_code:
+                self.datas.remove(data)
+                return True
+        return False
+
+    def update(self, data_code, **kwargs):
+        for data in self.datas:
+            if data.primary_key != data_code: continue
+            for key, value in kwargs.items():
+                try:
+                    if kwargs[key] is not None: setattr(data, key, value)
+                except KeyError:
+                    raise KeyError("Error From class BaseManager: kwargs got parameter that isn't exist in your Entity")
+            return True
+        return False
+
+    def get(self, data_code):
+        for data in self.datas:
+            if data.primary_key == data_code:
+                return data
+        return None
+
+class BaseEntity(ABC):
+    @property
+    @abstractmethod
+    def primary_key(self):
+        """
+        这里需要继承的子类返回自己的主键
+        """
+        pass
+
+    @abstractmethod
+    def convert_to_list(self):
+        """
+        这里需要继承的子类把自己的数据转换为列表
+        """
+        pass
 
 
-class Movie:
+class Movie(BaseEntity):
     def __init__(self,movie_code,movie_name,cinema_number,cinema_start_time,cinema_end_time,date,original_price,discount):
-        self._date = None
-        self._discount = None
-        self._original_price = None
         self.movie_code = movie_code
         self.movie_name = movie_name
         self.cinema_number = cinema_number
@@ -32,44 +79,20 @@ class Movie:
         return '\n'.join(information)
 
     @property
+    def primary_key(self):
+        return self.movie_code
+
+    @property
     def original_price(self):
         return self._original_price
-
-    @original_price.setter
-    def original_price(self, value):
-        try:
-            int(value)
-        except ValueError:
-            raise ValueError("Error From class Movie: Please enter numeric value")
-        self._original_price = int(value)
 
     @property
     def discount(self):
         return self._discount
 
-    @discount.setter
-    def discount(self, value):
-        try:
-            int(value)
-        except ValueError:
-            raise ValueError("Error From class Movie: Please enter numeric value")
-        if int(value) > 100 or int(value) < 0:
-            raise ValueError("Error From class Movie: discount must be between 0 and 100")
-        self._discount = int(value)
-
     @property
     def date(self):
         return self._date
-
-    @date.setter
-    def date(self, value):
-        try:
-            datetime.strptime(value, "%Y/%m/%d")
-        except ValueError:
-            raise ValueError("Error From class Movie: Invalid date (format YY/MM/DD)")
-        except Exception as e:
-            raise Exception(f"Unknown Error From class Movie: {e}")
-        self._date = str(value)
 
     @property
     def _start_time(self):
@@ -95,61 +118,87 @@ class Movie:
             minutes += 24 * 60
         return minutes
 
+    @original_price.setter
+    def original_price(self, value):
+        try:
+            int(value)
+        except ValueError:
+            raise ValueError("Error From class Movie: Please enter numeric value")
+        self._original_price = int(value)
 
-class MovieManager:
+    @discount.setter
+    def discount(self, value):
+        try:
+            int(value)
+        except ValueError:
+            raise ValueError("Error From class Movie: Please enter numeric value")
+        if int(value) > 100 or int(value) < 0:
+            raise ValueError("Error From class Movie: discount must be between 0 and 100")
+        self._discount = int(value)
+
+    @date.setter
+    def date(self, value):
+        try:
+            datetime.strptime(value, "%Y/%m/%d")
+        except ValueError:
+            raise ValueError("Error From class Movie: Invalid date (format YY/MM/DD)")
+        except Exception as e:
+            raise Exception(f"Unknown Error From class Movie: {e}")
+        self._date = str(value)
+
+    def convert_to_list(self):
+        return [
+            self.movie_code,self.movie_name,self.cinema_number,self.cinema_start_time,
+            self.cinema_end_time,self.date,self.original_price,self.discount
+        ]
+
+
+class MovieManager(BaseManager):
     def __init__(self):
-        self.movies = []
+        super().__init__()
 
     def __str__(self):
         information = []
-        for movie in self.movies:
+        for movie in self.datas:
             information.append(str(movie))
-        return '\n'.join(information)
+        return '\n\n'.join(information)
 
-    def find_movie(self, movie_code):
-        for movie in self.movies:
-            if movie.movie_code == movie_code:
-                return movie
-        raise ValueError ("Error From MovieManager: Movie not found (find_movie)")
+    def add_for_non_entity(self,movie_code, movie_name, cinema_number, cinema_start_time, cinema_end_time, date, original_price,
+                   discount):
+        movie = Movie(movie_code, movie_name, cinema_number, cinema_start_time, cinema_end_time, date, original_price,
+                     discount)
+        super().add(movie)
 
-    def add_movie(self,movie_code,movie_name,cinema_number,cinema_start_time,cinema_end_time,date,original_price,discount):
-        if self.repeat_code_check(movie_code): raise ValueError("Error From class MovieManager: Movie Code Already Exists")
-        self.movies.append(
-            Movie(movie_code,movie_name,cinema_number,cinema_start_time,cinema_end_time,date,original_price,discount)
-        )
-
-    def remove_movie(self,movie_code):
-        for movie in self.movies:
-            if movie.movie_code == movie_code:
-                self.movies.remove(movie)
-                return True
+    def remove(self, movie_code):
+        if super().remove(movie_code): return True
         raise ValueError(f"Error From class MovieManager: Movie Code Not Found (remove_movie)")
 
-    def update_movie(
+    def update(
             self,movie_code,movie_name=None,cinema_number=None,cinema_start_time=None,
             cinema_end_time=None,date=None,original_price=None,discount=None
     ):
-            for index,movie in enumerate(self.movies):
-                if movie.movie_code == movie_code:
-                    if movie_name is not None: movie.movie_name = movie_name
-                    if cinema_number is not None: movie.cinema_number = cinema_number
-                    if cinema_start_time is not None: movie.cinema_start_time = cinema_start_time
-                    if cinema_end_time is not None: movie.cinema_end_time = cinema_end_time
-                    if date is not None: movie.date = date
-                    if original_price is not None: movie.original_price = original_price
-                    if discount is not None: movie.discount = discount
+        super().update(
+            data_code=movie_code, movie_name=movie_name, cinema_number=cinema_number,
+            cinema_start_time=cinema_start_time, cinema_end_time=cinema_end_time, original_price=original_price,
+            discount=discount
+        )
 
-    def repeat_code_check(self,movie_code):
-        for movie in self.movies:
-            if movie.movie_code == movie_code:
-                return True
-        return False
+class FileManager:
+    def __init__(self,file_path):
+        self.file_path = file_path
 
-# if __name__ == "__main__":
-#     movie = Movie(
-#         'M001','Joker','0001','18:00','20:00',
-#                   '2025/01/12','30','25'
-#     )
-#     print(movie)
+    def write_to_csv(self,movies):
+        with open(self.file_path,"w",newline="") as csv_file:
+            writer = csv.writer(csv_file)
+            for movie in movies:
+                writer.writerow(movie.get_data_for_file())
+
+if __name__ == "__main__":
+     movie_all = MovieManager()
+     movie_all.add_for_non_entity('M001', 'Joker', '0001', '18:00', '20:00',
+                   '2025/01/12','30','25')
+     movie_all.add_for_non_entity('M002', 'Joker', '0001', '18:00', '20:00',
+                   '2025/01/12','30','25')
+     print(movie_all)
 
 
