b29414f4fb958a3ab1e3eea969fb6c8f2900ca03
BGORE <leezhihao0427@gmail.com>
2025-11-23 00:47:58 +0800

    修改了Movie的名字为MovieEntity，test.py受到影响也需要更改

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from Handler import *
 
 if __name__ == "__main__":
-    movie_file = ObjectFileHandler("movie.csv",Movie)
+    movie_file = ObjectFileHandler("movie.csv", MovieEntity)
     movie_manager = MovieManager(movie_file.get_from_csv())
     print(movie_manager)
 
