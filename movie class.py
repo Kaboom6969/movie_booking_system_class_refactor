213ddc7ff90ccab8fd3070c220712776d5d2a1d2
BGORE <leezhihao0427@gmail.com>
2025-11-22 16:03:50 +0800

    测试三个类的沟通是否正确

--- movie class.py
+++ movie class.py
@@ -1,11 +1,8 @@
-from Manager import *
+from Handler import *
 
 if __name__ == "__main__":
-     movie_all = MovieManager()
-     movie_all.add_for_non_entity('M001', 'Joker', '0001', '18:00', '20:00',
-                   '2025/01/12','30','25')
-     movie_all.add_for_non_entity('M002', 'Joker', '0001', '18:00', '20:00',
-                   '2025/01/12','30','25')
-     print(movie_all)
+    movie_file = ObjectFileHandler("movie.csv",Movie)
+    movie_manager = MovieManager(movie_file.get_from_csv())
+    print(movie_manager)
 
 
